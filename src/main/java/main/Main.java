4704b489be6161531be21093355515c0eee7c1a6
John Cho <yohancho316@gmail.com>
2024-06-18 23:00:54 -0700

    Started Design of Each Forecast Node

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,15 +1,42 @@
 package main;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+
+        // HBOX Configuration
+        double HBOX_NODE_SPACING = 1.0;
+
+        // HBox Configurations
+        HBox hbox = new HBox();
+
+        // Configure Spacing between Child Nodes to 1.0 Pixels
+        hbox.setSpacing(HBOX_NODE_SPACING);
+
+        // Configure Alignment Between Child Nodes to Left Center
+        hbox.setAlignment(Pos.CENTER_LEFT);
+
+
+
+
+
+
+
+
+
+
+
+
+
         BorderPane pane = new BorderPane();
         Button btn = new Button("Hello World");
         pane.setCenter(btn);
