b6944bfe3ba005402062223fbefc7235bef8d2f6
John Cho <yohancho316@gmail.com>
2024-06-21 10:23:02 -0700

    defined BorderPane configuration properties

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -4,9 +4,11 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -25,7 +27,7 @@ public class Main extends Application {
         double HBOX_MAX_HEIGHT = 200;
         Insets HBOX_PADDING = new Insets(0.0);
 
-        // HBox Configurations
+        // Instantiate HBox Layout Wrapper Node
         HBox hbox = new HBox();
 
         // Configure Spacing between Child Nodes to 1.0 Pixels
@@ -54,8 +56,59 @@ public class Main extends Application {
 
 //////// BorderPane ////////////////////////////////////////////////////
 
-        // HBox Layout Wrapper Enumerations
-        
+        // BorderPane Layout Wrapper Enumerations
+        double BORDER_PANE_MIN_WIDTH = 120;
+        double BORDER_PANE_MAX_WIDTH = 120;
+        double BORDER_PANE_PREF_WIDTH = 120;
+        double BORDER_PANE_MIN_HEIGHT = 200;
+        double BORDER_PANE_MAX_HEIGHT = 200;
+        double BORDER_PANE_PREF_HEIGHT = 200;
+        Insets BORDER_PANE_INSETS = new Insets(0.0);
+
+        // Instantiate BorderPane Layout Wrapper Node
+        BorderPane borderPane = new BorderPane();
+
+        // Configure Minimum Width of Border Pane Layout Wrapper
+        borderPane.setMinWidth(BORDER_PANE_MIN_WIDTH);
+
+        // Configure Maximum Width of BorderPane Layout Wrapper
+        borderPane.setMaxWidth(BORDER_PANE_MAX_WIDTH);
+
+        // Configure Preferred Width of BorderPane Layout Wrapper
+        borderPane.setPrefWidth(BORDER_PANE_PREF_WIDTH);
+
+        // Configure Minimum Height of BorderPane Layout Wrapper
+        borderPane.setMinHeight(BORDER_PANE_MIN_HEIGHT);
+
+        // Configure Maximum Height of BorderPane Layout Wrapper
+        borderPane.setMaxHeight(BORDER_PANE_MAX_HEIGHT);
+
+        // Configure Preferred Height of BorderPane Layout Wrapper
+        borderPane.setPrefHeight(BORDER_PANE_PREF_HEIGHT);
+
+        // Configure Minimum Height of BorderPane Layout Wrapper
+        borderPane.setMinHeight(BORDER_PANE_MIN_HEIGHT);
+
+        // Configure Maximum Height of BorderPane Layout Wrapper
+        borderPane.setMaxHeight(BORDER_PANE_MAX_HEIGHT);
+
+        // Configure Padding Height & Width of BorderPane Wrapper
+        borderPane.setPadding(BORDER_PANE_INSETS);
+
+        // Configure Background Color of BorderPane Layout Wrappers
+        int red = 50;
+        int green = 50;
+        int blue = 50;
+        Color rgb = Color.rgb(red, green, blue);
+        BackgroundFill backgroundFill = new BackgroundFill(rgb, null, null);
+        Background background = new Background(backgroundFill);
+        borderPane.setBackground(background);
+
+////////////////////////////////////////////////////////////////////////
+
+
+        // Add BorderPane Layout Nodes within HBox Layout Wrapper
+        hbox.getChildren().add(borderPane);
 
 
 
@@ -66,10 +119,9 @@ public class Main extends Application {
 
 
 
-        BorderPane pane = new BorderPane();
-        Button btn = new Button("Hello World");
-        pane.setCenter(btn);
-        primaryStage.setScene(new Scene(pane));
+
+
+        primaryStage.setScene(new Scene(hbox));
         primaryStage.show();
     }
 
