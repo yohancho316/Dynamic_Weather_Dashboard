5c35c81454077e9fc53e06a2c7c02edbc4bfc793
John Cho <yohancho316@gmail.com>
2024-07-22 20:46:29 -0700

    Updated checks for API key retrieval

--- src/main/java/model/ApiKeyReader.java
+++ src/main/java/model/ApiKeyReader.java
@@ -7,6 +7,10 @@ public class ApiKeyReader {
     private static final String API_KEY_FILE_PATH = "apikey.txt"; // No leading slash
 
     public static String getKey() {
+
+        if(API_KEY_FILE_PATH == null) throw new NullPointerException("API File Path cannot be null");
+        if(API_KEY_FILE_PATH.isEmpty()) throw new IllegalArgumentException("API File Path cannot be empty");
+
         StringBuilder apiKey = new StringBuilder();
         ClassLoader classLoader = ApiKeyReader.class.getClassLoader();
 
@@ -25,4 +29,4 @@ public class ApiKeyReader {
 
         return apiKey.toString().trim();
     }
-    }
+}
