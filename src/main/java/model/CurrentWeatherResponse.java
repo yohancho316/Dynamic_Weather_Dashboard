0db57fb4b43313ed33fcdfcb9b76504d8d20e1d2
John Cho <yohancho316@gmail.com>
2024-07-24 22:42:21 -0700

    continued to implement the CurrentWeatherResponse class definition

--- src/main/java/model/CurrentWeatherResponse.java
+++ src/main/java/model/CurrentWeatherResponse.java
@@ -6,10 +6,25 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public class CurrentWeatherResponse {
 
     @JsonProperty("coord")
-    Coord coordinates;
+    private Coord coordinates;
 
     @JsonProperty("weather")
-    List<Weather> weather; 
+    private List<Weather> weather;
+
+    @JsonProperty("base")
+    private String base;
+
+    @JsonProperty("main")
+    private Main main;
+
+    @JsonProperty("visibility")
+    private double visibility;
+
+    @JsonProperty("wind")
+    private Wind wind;
+
+    @JsonProperty("clouds")
+    private Clouds clouds;
 
     // Nested Static Coord Class
     public static class Coord {
@@ -109,7 +124,162 @@ public class CurrentWeatherResponse {
 
     }
 
+    // Nested Static Main Class
+    public static class Main {
 
+        @JsonProperty("temp")
+        private double currentTemperature;
+
+        @JsonProperty("feels_like")
+        private double feels_like;
+
+        @JsonProperty("temp_min")
+        private double temp_min;
+
+        @JsonProperty("temp_max")
+        private double temp_max;
+
+        @JsonProperty("pressure")
+        private double pressure;
+
+        @JsonProperty("humidity")
+        private double humidity;
+
+        @JsonProperty("sea_level")
+        private double sea_level;
+
+        @JsonProperty("grnd_level")
+        private double grnd_level;
+
+        // Current Temperature Getter Method
+        public double getCurrentTemperature() {
+            return this. currentTemperature;
+        }
+
+        // Current Temperature Setter Method
+        public void setCurrentTemperature(double currentTemperature) {
+            this.currentTemperature = currentTemperature;
+        }
+
+        // Feels Like Temperature Getter Method
+        public double getFeelsLikeTemperature() {
+            return this.feels_like;
+        }
+
+        // Feels Like Temperature Setter Method
+        public void setFeelsLikeTemperature(double feels_like) {
+            this.feels_like = feels_like;
+        }
+
+        // Min Temp Getter Method
+        public double getMinTemp() {
+            return this.temp_min;
+        }
+
+        // Min Temp Setter Method
+        public void setMinTemp(double temp_min) {
+            this.temp_min = temp_min;
+        }
+
+        // Max Temp Getter Method
+        public double getMaxTemp() {
+            return this.temp_max;
+        }
+
+        // Max Temp Setter Method
+        public void setMaxTemp(double temp_max) {
+            this.temp_max = temp_max;
+        }
+
+        // Pressure Getter Method
+        public double getPressure() {
+            return this.pressure;
+        }
+
+        // Pressure Setter Method
+        public void setPressure(double pressure) {
+            this.pressure = pressure;
+        }
+
+        // Humidity Getter Method
+        public double getHumidity() {
+            return this.humidity;
+        }
+
+        // Humidity Setter Method
+        public void setHumidity(double humidity) {
+            this.humidity = humidity;
+        }
+
+        // Sea Level Getter Method
+        public double getSeaLevel() {
+            return this.sea_level;
+        }
+
+        // Sea Level Setter Method
+        public void setSeaLevel(double sea_level) {
+            this.sea_level = sea_level;
+        }
+
+        // Ground Level Getter Method
+        public double getGroundLevel() {
+            return this.grnd_level;
+        }
+
+        // Ground Level Setter Method
+        public void setGroundLevel(double grnd_level) {
+            this.grnd_level = grnd_level;
+        }
+
+
+    }
+
+    // Nested Static Wind Class
+    public static class Wind {
+
+        @JsonProperty("speed")
+        private double speed;
+
+        @JsonProperty("deg")
+        private double degree;
+
+        // Speed Getter Method
+        public double getSpeed() {
+            return this.speed;
+        }
+
+        // Speed Setter Method
+        public void setSpeed(double speed) {
+            this.speed = speed;
+        }
+
+        // Degrees Getter Method
+        public double getDegree() {
+            return this.degree;
+        }
+
+        // Degrees Setter Method
+        public void setDegree(double degree) {
+            this.degree = degree;
+        }
+    }
+
+    // Nested Static Cloud Class
+    public static class Clouds {
+
+        @JsonProperty("all")
+        private double all;
+
+        // All Getter Method
+        public double getAll() {
+            return this.all;
+        }
+
+        // All Setter Method
+        public void setAll(double all) {
+            this.all = all;
+        }
+    }
 
 
 }
