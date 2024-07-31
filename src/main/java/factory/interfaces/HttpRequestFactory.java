cbad9afd14513a50fad244413a218567ab7a4fa3
John Cho <yohancho316@gmail.com>
2024-07-31 16:29:04 -0700

    Implemented HttpRequest Object Factory Concrete Definitions

--- src/main/java/factory/interfaces/HttpRequestFactory.java
+++ src/main/java/factory/interfaces/HttpRequestFactory.java
@@ -1,7 +1,9 @@
 package factory.interfaces;
 import model.ApiKeyReader;
+import model.GeocodingResponse;
 
 import java.net.http.HttpRequest;
+import java.util.List;
 
 public interface HttpRequestFactory {
 
@@ -10,19 +12,18 @@ public interface HttpRequestFactory {
     String GEOCODE_BASE_URL = "https://api.openweathermap.org/geo/1.0/direct";
     String CURRENT_BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
     String FUTURE_BASE_URL = "https://api.openweathermap.org/data/2.5/forecast";
-    String GEOCODE_CITY_NAME = "Los Angeles";
     String GEOCODE_METRIC_SYSTEM = "imperial";
     String CURRENT_METRIC_SYSTEM = "imperial";
     String FUTURE_METRIC_SYSTEM = "imperial";
     int GEOCODE_RESPONSE_LIMIT = 1;
 
     // Geocode HTTP Request Object Factory Method Signature
-    public HttpRequest createGeocodeRequest();
+    public HttpRequest createGeocodeRequest(String CITY_NAME);
 
     // Current Forecast HTTP Request Object Factory Method Signature
-    public HttpRequest createCurrentRequest();
+    public HttpRequest createCurrentRequest(String LATITUDE, String LONGITUDE);
 
     // Future Forecast HTTP Request Object Factory Method Signature
-    public HttpRequest createFutureRequest();
-    
+    public HttpRequest createFutureRequest(String LATITUDE, String LONGITUDE);
+
 }
